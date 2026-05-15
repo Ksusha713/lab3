9d0a105d64005c7d25676f2dd16ffcdfc576dd9e
KseniaShustikova <73019085+KseniaShustikova@users.noreply.github.com>
2026-05-15 22:47:31 +0200

    fixed dbs

--- data/coursesdb.py
+++ data/coursesdb.py
@@ -1,8 +1,13 @@
 import sqlite3
 
-connection = sqlite3.connect("courses.db")
-cursor = connection.cursor()
+def db_connection():
+    connection = sqlite3.connect("courses.db")
+    return connection
 
-cursor.execute("PRAGMA foreign_keys = ON")
-
-cursor.execute("CREATE TABLE IF NOT EXISTS courses(courseID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, teacher TEXT NOT NULL, credits INTEGER NOT NULL, semester INTEGER NOT NULL)")
+def create_db():
+    connection = db_connection()
+    cursor = connection.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.execute("CREATE TABLE IF NOT EXISTS courses(courseID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, teacher TEXT NOT NULL, credits INTEGER NOT NULL, semester INTEGER NOT NULL)")
+    connection.commit()
+    connection.close()
--- data/usersdb.py
+++ data/usersdb.py
@@ -1,10 +1,15 @@
 import sqlite3
 import bcrypt
 
-connection = sqlite3.connect("users.db")
-cursor = connection.cursor()
+def db_connection():
+    connection = sqlite3.connect("users.db")
+    return connection
 
-cursor.execute("PRAGMA foreign_keys = ON")
-
-cursor.execute("CREATE TABLE IF NOT EXISTS users(userID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL, password TEXT NOT NULL)")
+def create_db():
+    connection = db_connection()
+    cursor = connection.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.execute("CREATE TABLE IF NOT EXISTS users(userID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL, password TEXT NOT NULL)")
+    connection.commit()
+    connection.close()
 
